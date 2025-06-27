e28c00a63c74d4447ed46231e1cd3e6a90d36302
FrekeNobles <noblendifreke@gmail.com>
2025-06-27 18:46:40 +0100

    feat.:add routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddPostPage from "./pages/AddPostPage.jsx"
+import AddPostPage from "./pages/AllPostPage.jsx"
+import AddPostPage from "./pages/PostDetailPage.jsx"
+
 
 
 function App() {
@@ -9,7 +13,9 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path='' element={<Form/>}/>
+          <Route path='/' element={<AllPostPage/>}/>
+          <Route path='/addpost' element={<AddPostPage/>}/>
+          <Route path='/postdetail' element={<PostDetailPage/>}/>
         </Routes>
       </Router>
     </>
