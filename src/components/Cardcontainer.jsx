745d93b9f597330534f96592c74a72f646ba4b20
omodotetteh <141946470+omodotetteh@users.noreply.github.com>
2025-06-27 12:18:50 +0100

    cardcontainer

---
+++ src/components/Cardcontainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cardcontainer() {
+  return (
+    <div className='border border-black flex flex-row @sm:@max-md:flex-col justify-center items-center h-screen'>
+      
+    </div>
+  )
+}
+
+export default Cardcontainer
