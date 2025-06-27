9a2c026852ce7d80841b7eabe7ff8340798736cf
clement <clemcy9@gmail.com>
2025-06-27 13:53:56 +0100

    update: add fetch api to allpostpage

--- src/pages/AllPostPage.jsx
+++ src/pages/AllPostPage.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import Cardcontainer from '../components/Cardcontainer'
-import Card from '..components/Card'
+import React, { useEffect } from "react";
+import Cardcontainer from "../components/Cardcontainer";
+import Card from "..components/Card";
 
 function AllPostPage() {
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => console.log(error));
+  }, []);
   return (
     <div>
       <Cardcontainer>
-        <Card/>
-         <Card/>
+        <Card />
+        <Card />
       </Cardcontainer>
     </div>
-  )
+  );
 }
 
-export default AllPostPage
+export default AllPostPage;
